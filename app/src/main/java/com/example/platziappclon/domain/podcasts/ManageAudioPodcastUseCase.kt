6483f1ade88a68abaa413fcb8552f9840186e8f8
neolink557 <neolink557@gmail.com>
2022-02-25 23:04:16 -0500

    podcast dialog working at 70% o(*￣▽￣*)ブ

--- app/src/main/java/com/example/platziappclon/domain/podcasts/ManageAudioPodcastUseCase.kt
+++ app/src/main/java/com/example/platziappclon/domain/podcasts/ManageAudioPodcastUseCase.kt
@@ -2,9 +2,11 @@ package com.example.platziappclon.domain.podcasts
 
 import android.media.AudioManager
 import android.media.MediaPlayer
+import androidx.lifecycle.viewModelScope
 import com.example.platziappclon.data.Repository.PodcastsRepository
 import com.example.platziappclon.data.model.PodcastsModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.IOException
 import javax.inject.Inject
@@ -22,9 +24,13 @@ class ManageAudioPodcastUseCase @Inject constructor(
         return withContext(Dispatchers.IO) {
             try {
                 if (!mediaPlayer.isPlaying) {
-                    mediaPlayer.start()
+                    mediaPlayer.setOnPreparedListener {
+                        mediaPlayer.start()
+                    }
                 } else {
-                    mediaPlayer.stop()
+                    mediaPlayer.setOnCompletionListener {
+                        mediaPlayer.stop()
+                    }
                 }
 
             } catch (e: IOException) {
