1d5ceec2d177782513ffe1cabd68bc8b2859f6f2
neolink557 <neolink557@gmail.com>
2022-03-20 20:53:36 -0500

    time of podcasts fixed

--- app/src/main/java/com/example/platziappclon/domain/podcasts/ManageAudioPodcastUseCase.kt
+++ app/src/main/java/com/example/platziappclon/domain/podcasts/ManageAudioPodcastUseCase.kt
@@ -72,13 +72,11 @@ class ManageAudioPodcastUseCase @Inject constructor(
     }
 
     private fun convertFormat(time: Long): String {
-        return String.format(
-            "%02d:%02d",
-            TimeUnit.MILLISECONDS.toMinutes(time),
-            TimeUnit.MILLISECONDS.toSeconds(time),
-            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))
-        )
+        val minutes = TimeUnit.MINUTES.convert(time, TimeUnit.MILLISECONDS)
+        val seconds = (TimeUnit.SECONDS.convert(time, TimeUnit.MILLISECONDS)
+                - minutes * TimeUnit.SECONDS.convert(1, TimeUnit.MINUTES))
 
+        return String.format("%02d:%02d", minutes, seconds)
     }
 }
 
